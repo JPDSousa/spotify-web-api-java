1ab22ef8c2715d6baa8d174c63de8b07d0ea09d2
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-11-18 10:05:28 +0000

    json factory for simple artist

---
+++ src/main/java/com/wrapper/spotify/models/artist/SimpleArtistJsonFactory.java
@@ -0,0 +1,25 @@
+package com.wrapper.spotify.models.artist;
+
+import com.wrapper.spotify.models.json.SpotifyModelJsonFactory;
+
+import net.sf.json.JSONObject;
+
+@SuppressWarnings("javadoc")
+public class SimpleArtistJsonFactory extends SpotifyModelJsonFactory<SimpleArtist> {
+
+	private static final String NAME = "name";
+
+	@Override
+	public SimpleArtist fromJson(JSONObject jsonObject) {
+		final SimpleArtist simpleArtist = new SimpleArtist(getId(jsonObject));
+
+		return simpleArtist;
+	}
+
+	@Override
+	protected void fillObject(SimpleArtist baseObject, JSONObject jsonObject) {
+		super.fillObject(baseObject, jsonObject);
+		baseObject.setName(jsonObject.getString(NAME));
+	}
+	
+}
