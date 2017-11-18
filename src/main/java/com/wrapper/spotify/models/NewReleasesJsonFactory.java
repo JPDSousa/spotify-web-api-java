372701b0dde7feca9bcdb04a5b1cbb3a242dffe1
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-11-18 12:24:41 +0000

    json factory for new releases

---
+++ src/main/java/com/wrapper/spotify/models/NewReleasesJsonFactory.java
@@ -0,0 +1,27 @@
+package com.wrapper.spotify.models;
+
+import com.wrapper.spotify.json.AbstractJsonFactory;
+import com.wrapper.spotify.models.album.SimpleAlbum;
+import com.wrapper.spotify.models.page.PageJsonFactory;
+
+import net.sf.json.JSONObject;
+
+@SuppressWarnings("javadoc")
+public class NewReleasesJsonFactory extends AbstractJsonFactory<NewReleases> {
+
+	private static final String ALBUMS = "albums";
+	
+	private final PageJsonFactory<SimpleAlbum> pageJsonFactory;
+	
+	public NewReleasesJsonFactory() {
+		pageJsonFactory = new PageJsonFactory<>();
+	}
+
+	@Override
+	public NewReleases fromJson(JSONObject jsonObject) {
+		final NewReleases newReleases = new NewReleases();
+		newReleases.setAlbums(pageJsonFactory.fromJson(jsonObject.getJSONObject(ALBUMS)));
+		return newReleases;
+	}
+
+}
