c6bfeb54f979effb48abc603624526083f82f3f3
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-11-18 15:13:01 +0000

    indentation

--- src/main/java/com/wrapper/spotify/HttpManager.java
+++ src/main/java/com/wrapper/spotify/HttpManager.java
@@ -10,41 +10,41 @@ import java.io.IOException;
  */
 public interface HttpManager {
 
-  /**
-   * Perform an HTTP GET request to the specified URL.
-   *
-   * @param url the {@link Url} to HTTP GET.
-   * @return a String containing the body of the HTTP GET response.
-   * @throws WebApiException In case of error replies from the Web API.
-   * @throws IOException In case of networking issues.
-   */
-  String get(Url url) throws WebApiException, IOException;
+	/**
+	 * Perform an HTTP GET request to the specified URL.
+	 *
+	 * @param url the {@link Url} to HTTP GET.
+	 * @return a String containing the body of the HTTP GET response.
+	 * @throws WebApiException In case of error replies from the Web API.
+	 * @throws IOException In case of networking issues.
+	 */
+	String get(Url url) throws WebApiException, IOException;
 
-  /**
-   * Perform an HTTP POST request to the specified URL.
-   *
-   * @param url the {@link Url} to HTTP POST.
-   * @return a String containing the body of the HTTP POST response.
-   * @throws WebApiException In case of error replies from the Web API.
-   * @throws IOException In case of networking issues.
-   */
-  String post(Url url) throws WebApiException, IOException;
+	/**
+	 * Perform an HTTP POST request to the specified URL.
+	 *
+	 * @param url the {@link Url} to HTTP POST.
+	 * @return a String containing the body of the HTTP POST response.
+	 * @throws WebApiException In case of error replies from the Web API.
+	 * @throws IOException In case of networking issues.
+	 */
+	String post(Url url) throws WebApiException, IOException;
 
-  /**
-   * Perform an HTTP DELETE request to the specified URL.
-   * @param url the {@link Url} to HTTP DELETE.
-   * @return a String containing the body of the HTTP DELETE response.
-   */
-  String delete(Url url) throws WebApiException, IOException;
+	/**
+	 * Perform an HTTP DELETE request to the specified URL.
+	 * @param url the {@link Url} to HTTP DELETE.
+	 * @return a String containing the body of the HTTP DELETE response.
+	 */
+	String delete(Url url) throws WebApiException, IOException;
 
-  /**
-   * Perform an HTTP PUT request to the specified URL.
-   *
-   * @param url the {@link Url} to HTTP PUT.
-   * @return a String containing the body of the HTTP PUTresponse.
-   * @throws WebApiException In case of error replies from the Web API.
-   * @throws IOException In case of networking issues.
-   */
-  String put(Url url) throws IOException, WebApiException;
+	/**
+	 * Perform an HTTP PUT request to the specified URL.
+	 *
+	 * @param url the {@link Url} to HTTP PUT.
+	 * @return a String containing the body of the HTTP PUTresponse.
+	 * @throws WebApiException In case of error replies from the Web API.
+	 * @throws IOException In case of networking issues.
+	 */
+	String put(Url url) throws IOException, WebApiException;
 
 }
--- src/main/java/com/wrapper/spotify/SpotifyHttpManager.java
+++ src/main/java/com/wrapper/spotify/SpotifyHttpManager.java
@@ -26,181 +26,182 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("javadoc")
 public class SpotifyHttpManager implements HttpManager {
 
-  private HttpClientConnectionManager connectionManager = null;
+	private HttpClientConnectionManager connectionManager = null;
 
-  /**
-   * Construct a new SpotifyHttpManager instance.
-   * @param builder The builder.
-   */
-  public SpotifyHttpManager(Builder builder) {
-    if (builder.connectionManager != null) {
-      connectionManager = builder.connectionManager;
-    } else {
-      connectionManager = new PoolingHttpClientConnectionManager();
-    }
-  }
+	/**
+	 * Construct a new SpotifyHttpManager instance.
+	 * @param builder The builder.
+	 */
+	public SpotifyHttpManager(Builder builder) {
+		if (builder.connectionManager != null) {
+			connectionManager = builder.connectionManager;
+		} else {
+			connectionManager = new PoolingHttpClientConnectionManager();
+		}
+	}
 
-  @Override
-  public String get(Url url) throws WebApiException, IOException {
-    assert (url != null);
+	@Override
+	public String get(Url url) throws WebApiException, IOException {
+		assert (url != null);
 
-    final String uri = UrlUtil.assemble(url);
-    final HttpGet method = new HttpGet(uri);
+		final String uri = UrlUtil.assemble(url);
+		final HttpGet method = new HttpGet(uri);
 
-    for (Url.Parameter header : url.getHeaderParametersList()) {
-      method.setHeader(header.getName(), header.getValue());
-    }
+		for (Url.Parameter header : url.getHeaderParametersList()) {
+			method.setHeader(header.getName(), header.getValue());
+		}
 
-    return execute(method);
-  }
+		return execute(method);
+	}
 
-  @Override
-  public String post(UtilProtos.Url url) throws IOException, WebApiException {
-    assert (url != null);
+	@Override
+	public String post(UtilProtos.Url url) throws IOException, WebApiException {
+		assert (url != null);
 
-    final String uri = UrlUtil.assemble(url);
-    final HttpPost method = new HttpPost(uri);
+		final String uri = UrlUtil.assemble(url);
+		final HttpPost method = new HttpPost(uri);
 
-    for (Url.Parameter header : url.getHeaderParametersList()) {
-      method.setHeader(header.getName(), header.getValue());
-    }
+		for (Url.Parameter header : url.getHeaderParametersList()) {
+			method.setHeader(header.getName(), header.getValue());
+		}
 
-    if (url.hasJsonBody()) {
-      StringEntity stringEntity = new StringEntity(
-              url.getJsonBody(),
-              ContentType.APPLICATION_JSON);
-      method.setEntity(stringEntity);
-    } else {
-      method.setEntity(new UrlEncodedFormEntity(getBodyParametersAsNamedValuePairArray(url)));
-    }
+		if (url.hasJsonBody()) {
+			StringEntity stringEntity = new StringEntity(
+					url.getJsonBody(),
+					ContentType.APPLICATION_JSON);
+			method.setEntity(stringEntity);
+		} else {
+			method.setEntity(new UrlEncodedFormEntity(getBodyParametersAsNamedValuePairArray(url)));
+		}
 
-    return execute(method);
-  }
+		return execute(method);
+	}
 
-  @Override
-  public String put(UtilProtos.Url url) throws IOException, WebApiException {
-    assert (url != null);
+	@Override
+	public String put(UtilProtos.Url url) throws IOException, WebApiException {
+		assert (url != null);
 
-    final String uri = UrlUtil.assemble(url);
-    final HttpPut method = new HttpPut(uri);
+		final String uri = UrlUtil.assemble(url);
+		final HttpPut method = new HttpPut(uri);
 
-    for (Url.Parameter header : url.getHeaderParametersList()) {
-      method.setHeader(header.getName(), header.getValue());
-    }
+		for (Url.Parameter header : url.getHeaderParametersList()) {
+			method.setHeader(header.getName(), header.getValue());
+		}
 
-    if (url.hasJsonBody()) {
-      StringEntity stringEntity = new StringEntity(
-              url.getJsonBody(),
-              ContentType.APPLICATION_JSON);
-      method.setEntity(stringEntity);
-    } else {
-      method.setEntity(new UrlEncodedFormEntity(getBodyParametersAsNamedValuePairArray(url)));
-    }
+		if (url.hasJsonBody()) {
+			StringEntity stringEntity = new StringEntity(
+					url.getJsonBody(),
+					ContentType.APPLICATION_JSON);
+			method.setEntity(stringEntity);
+		} else {
+			method.setEntity(new UrlEncodedFormEntity(getBodyParametersAsNamedValuePairArray(url)));
+		}
 
-    return execute(method);
-  }
+		return execute(method);
+	}
 
 
-  // TODO(michael): Allow JSON body to be sent.
-  @Override
-  public String delete(UtilProtos.Url url) throws IOException, WebApiException {
-    assert (url != null);
+	// TODO(michael): Allow JSON body to be sent.
+	@Override
+	public String delete(UtilProtos.Url url) throws IOException, WebApiException {
+		assert (url != null);
 
-    final String uri = UrlUtil.assemble(url);
-    final HttpDelete method = new HttpDelete(uri);
+		final String uri = UrlUtil.assemble(url);
+		final HttpDelete method = new HttpDelete(uri);
 
-    for (Url.Parameter header : url.getHeaderParametersList()) {
-      method.setHeader(header.getName(), header.getValue());
-    }
+		for (Url.Parameter header : url.getHeaderParametersList()) {
+			method.setHeader(header.getName(), header.getValue());
+		}
 
-    return execute(method);
-  }
+		return execute(method);
+	}
 
-  private List<NameValuePair> getBodyParametersAsNamedValuePairArray(Url url) {
-    List<NameValuePair> out = new ArrayList<>();
+	private List<NameValuePair> getBodyParametersAsNamedValuePairArray(Url url) {
+		List<NameValuePair> out = new ArrayList<>();
 
-    for (Url.Parameter parameter : url.getBodyParametersList()) {
-      if (parameter.hasName() && parameter.hasValue()) {
-        out.add(new BasicNameValuePair(parameter.getName(), parameter.getValue().toString()));
-      }
-    }
+		for (Url.Parameter parameter : url.getBodyParametersList()) {
+			if (parameter.hasName() && parameter.hasValue()) {
+				out.add(new BasicNameValuePair(parameter.getName(), parameter.getValue().toString()));
+			}
+		}
 
-    return out;
-  }
+		return out;
+	}
 
-  private String execute(HttpRequestBase method) throws WebApiException, IOException {
-    final ConnectionConfig connectionConfig = ConnectionConfig
-            .custom()
-            .setCharset(Charset.forName("UTF-8"))
-            .build();
-    final RequestConfig requestConfig = RequestConfig
-            .custom()
-            .setCookieSpec(CookieSpecs.DEFAULT)
-            .build();
-    final CloseableHttpClient httpClient = HttpClients.custom()
-            .setConnectionManager(connectionManager)
-            .setDefaultConnectionConfig(connectionConfig)
-            .setDefaultRequestConfig(requestConfig)
-            .build();
+	private String execute(HttpRequestBase method) throws WebApiException, IOException {
+		final ConnectionConfig connectionConfig = ConnectionConfig
+				.custom()
+				.setCharset(Charset.forName("UTF-8"))
+				.build();
+		final RequestConfig requestConfig = RequestConfig
+				.custom()
+				.setCookieSpec(CookieSpecs.DEFAULT)
+				.build();
+		final CloseableHttpClient httpClient = HttpClients.custom()
+				.setConnectionManager(connectionManager)
+				.setDefaultConnectionConfig(connectionConfig)
+				.setDefaultRequestConfig(requestConfig)
+				.build();
 
-    try {
-      CloseableHttpResponse httpResponse = httpClient.execute(method);
+		try {
+			CloseableHttpResponse httpResponse = httpClient.execute(method);
 
-      handleErrorStatusCode(httpResponse);
+			handleErrorStatusCode(httpResponse);
 
-      String responseBody = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
+			String responseBody = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
 
-      handleErrorResponseBody(responseBody);
+			handleErrorResponseBody(responseBody);
 
-      return responseBody;
-    } catch (IOException e) {
-      throw new IOException();
-    } finally {
-      method.releaseConnection();
-    }
-  }
+			return responseBody;
+		} catch (IOException e) {
+			throw new IOException();
+		} finally {
+			method.releaseConnection();
+		}
+	}
 
-  /*
-   * TODO: Error handling could be more granular and throw a different exception depending on status code.
-   * It could also look into the JSON object to find an error message.
-   */
-  private void handleErrorStatusCode(CloseableHttpResponse httpResponse) throws BadRequestException, ServerErrorException {
-    int statusCode = httpResponse.getStatusLine().getStatusCode();
+	/*
+	 * TODO: Error handling could be more granular and throw a different exception depending on status code.
+	 * It could also look into the JSON object to find an error message.
+	 */
+	private void handleErrorStatusCode(CloseableHttpResponse httpResponse) throws BadRequestException, ServerErrorException {
+		int statusCode = httpResponse.getStatusLine().getStatusCode();
 
-    if (statusCode >= 400 && statusCode < 500) {
-      throw new BadRequestException(String.valueOf(statusCode));
-    }
-    if (statusCode >= 500) {
-      throw new ServerErrorException(String.valueOf(statusCode));
-    }
+		if (statusCode >= 400 && statusCode < 500) {
+			throw new BadRequestException(String.valueOf(statusCode));
+		}
+		if (statusCode >= 500) {
+			throw new ServerErrorException(String.valueOf(statusCode));
+		}
 
-  }
+	}
 
-  private void handleErrorResponseBody(String responseBody) throws WebApiException {
-    if (responseBody == null) {
-      throw new EmptyResponseException("No response body");
-    }
+	private void handleErrorResponseBody(String responseBody) throws WebApiException {
+		if (responseBody == null) {
+			throw new EmptyResponseException("No response body");
+		}
 
-    if (!responseBody.equals("") && responseBody.startsWith("{")) {
-      final JSONObject jsonObject = JSONObject.fromObject(responseBody);
+		if (!responseBody.equals("") && responseBody.startsWith("{")) {
+			final JSONObject jsonObject = JSONObject.fromObject(responseBody);
 
-      if (jsonObject.has("error")) {
-        throw new WebApiException(jsonObject.getString("error"));
-      }
-    }
-  }
+			if (jsonObject.has("error")) {
+				throw new WebApiException(jsonObject.getString("error"));
+			}
+		}
+	}
 
-  public static Builder builder() {
-    return new Builder();
-  }
+	public static Builder builder() {
+		return new Builder();
+	}
 
-  public static class Builder {
-    private PoolingHttpClientConnectionManager connectionManager = null;
-    public Builder() {}
-    public SpotifyHttpManager build() {
-      return new SpotifyHttpManager(this);
-    }
-  }
+	public static class Builder {
+		private PoolingHttpClientConnectionManager connectionManager = null;
+		public Builder() {}
+		public SpotifyHttpManager build() {
+			return new SpotifyHttpManager(this);
+		}
+	}
 }
