a71e0d454ed01e618159e6998668506036e03f2c
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-11-18 10:01:48 +0000

    refactored with generics

--- src/main/java/com/wrapper/spotify/methods/Request.java
+++ src/main/java/com/wrapper/spotify/methods/Request.java
@@ -1,18 +1,27 @@
 package com.wrapper.spotify.methods;
 
+import java.io.IOException;
+
+import com.google.common.util.concurrent.SettableFuture;
 import com.wrapper.spotify.HttpManager;
 import com.wrapper.spotify.UtilProtos.Url;
+import com.wrapper.spotify.exceptions.WebApiException;
 
-public interface Request {
+@SuppressWarnings("javadoc")
+public interface Request<T> {
 
-  interface Builder {
-    Builder httpManager(HttpManager httpManager);
-    Builder host(String host);
-    Builder port(int port);
-    Builder scheme(Url.Scheme scheme);
-    AbstractRequest build();
-  }
+	interface Builder<E, T> {
+		Builder<E, T> httpManager(HttpManager httpManager);
+		Builder<E, T> host(String host);
+		Builder<E, T> port(int port);
+		Builder<E, T> scheme(Url.Scheme scheme);
+		Request<T> build();
+	}
 
-  Url toUrl();
+	T get() throws IOException, WebApiException;
+	
+	SettableFuture<T> getAsync();
+	
+	Url toUrl();
 
 }
