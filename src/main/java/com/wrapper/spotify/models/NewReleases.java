3fa0d61df370b1081c2d2a03c1433f44f2c3b2fc
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-11-19 11:03:16 +0000

    indentation

--- src/main/java/com/wrapper/spotify/models/NewReleases.java
+++ src/main/java/com/wrapper/spotify/models/NewReleases.java
@@ -3,14 +3,16 @@ package com.wrapper.spotify.models;
 import com.wrapper.spotify.models.album.SimpleAlbum;
 import com.wrapper.spotify.models.page.Page;
 
+@SuppressWarnings("javadoc")
 public class NewReleases {
-  private Page<SimpleAlbum> albums;
+	
+	private Page<SimpleAlbum> albums;
 
-  public Page<SimpleAlbum> getAlbums() {
-    return albums;
-  }
+	public Page<SimpleAlbum> getAlbums() {
+		return albums;
+	}
 
-  public void setAlbums(Page<SimpleAlbum> albums) {
-    this.albums = albums;
-  }
+	public void setAlbums(Page<SimpleAlbum> albums) {
+		this.albums = albums;
+	}
 }
