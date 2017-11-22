7e7b0f91a9bb45e64fb0bd8a1871123fab3e363f
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-11-22 10:56:04 +0000

    missing album type

--- src/main/java/com/wrapper/spotify/models/album/AlbumType.java
+++ src/main/java/com/wrapper/spotify/models/album/AlbumType.java
@@ -1,19 +1,21 @@
 package com.wrapper.spotify.models.album;
 
+@SuppressWarnings("javadoc")
 public enum AlbumType {
 
-  ALBUM("album"),
-  SINGLE("single"),
-  COMPILATION("compilation");
+	ALBUM("album"),
+	SINGLE("single"),
+	APPEARS_ON("appears_on"),
+	COMPILATION("compilation");
 
-  public final String type;
+	public final String type;
 
-  AlbumType(String type) {
-    this.type = type;
-  }
+	AlbumType(String type) {
+		this.type = type;
+	}
 
-  public String getType() {
-    return type;
-  }
+	public String getType() {
+		return type;
+	}
 
 }
