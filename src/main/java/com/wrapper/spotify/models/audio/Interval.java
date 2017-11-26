a513de1671b421275b4e9eb1f4929b1240dc8595
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-11-26 10:14:05 +0000

    interval

---
+++ src/main/java/com/wrapper/spotify/models/audio/Interval.java
@@ -0,0 +1,34 @@
+package com.wrapper.spotify.models.audio;
+
+@SuppressWarnings("javadoc")
+public class Interval {
+	
+	private float start;
+	private float duration;
+	private float confidence;
+	
+	public float getStart() {
+		return start;
+	}
+	
+	public void setStart(float start) {
+		this.start = start;
+	}
+	
+	public float getDuration() {
+		return duration;
+	}
+	
+	public void setDuration(float duration) {
+		this.duration = duration;
+	}
+	
+	public float getConfidence() {
+		return confidence;
+	}
+	
+	public void setConfidence(float confidence) {
+		this.confidence = confidence;
+	}
+
+}
