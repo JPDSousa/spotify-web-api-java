16913e902c9e12c240b5f3b6d387dd1ff6efcda8
João Sousa <jpd.sousa@campus.fct.unl.pt>
2018-09-23 12:44:58 +0100

    Enhanced Page Models

--- src/main/java/com/wrapper/spotify/models/page/Page.java
+++ src/main/java/com/wrapper/spotify/models/page/Page.java
@@ -1,89 +1,17 @@
 package com.wrapper.spotify.models.page;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import com.wrapper.spotify.json.JsonFactory;
+import org.immutables.gson.Gson;
+import org.immutables.value.Value;
 
-@SuppressWarnings("javadoc")
-public class Page<T> {
+@Value.Immutable
+@Gson.TypeAdapters
+public interface Page<T> extends GenericPage<T> {
 
-	private String href;
-	private List<T> items = new ArrayList<T>();
-	private int limit;
-	private String next;
-	private int offset;
-	private String previous;
-	private int total;
-	private final JsonFactory<T> jsonFactory;
-	private final String propName;
-	
-	public Page(String propName, JsonFactory<T> jsonFactory) {
-		this.jsonFactory = jsonFactory;
-		this.propName = propName;
-	}
-	
-	public JsonFactory<T> getJsonFactory() {
-		return jsonFactory;
-	}
-	
-	public String getPropName() {
-		return propName;
-	}
+    int offset();
 
-	public String getHref() {
-		return href;
-	}
+    Optional<String> previous();
 
-	public void setHref(String href) {
-		this.href = href;
-	}
-
-	public List<T> getItems() {
-		return items;
-	}
-
-	public void setItems(List<T> items) {
-		this.items = items;
-	}
-
-	public int getLimit() {
-		return limit;
-	}
-
-	public void setLimit(int limit) {
-		this.limit = limit;
-	}
-
-	public String getNext() {
-		return next;
-	}
-
-	public void setNext(String next) {
-		this.next = next;
-	}
-
-	public int getOffset() {
-		return offset;
-	}
-
-	public void setOffset(int offset) {
-		this.offset = offset;
-	}
-
-	public String getPrevious() {
-		return previous;
-	}
-
-	public void setPrevious(String previous) {
-		this.previous = previous;
-	}
-
-	public int getTotal() {
-		return total;
-	}
-
-	public void setTotal(int total) {
-		this.total = total;
-	}
 }
